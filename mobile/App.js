d8dfd06c9bcd7aeae713b9f4de0346aaf875bf52
okpalaChidiebere <okpalacollins4@gmail.com>
2021-09-29 21:07:31 -0600

    Using Google Places Autocomplete API
    FYI: we switched to using "showsUserLocation" from the MapView insted of using a Marker

--- mobile/App.js
+++ mobile/App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import MapView from "react-native-maps";
 import * as Location from "expo-location";
+import Constants from "expo-constants";
+import _ from "lodash";
 
+const apiKey = "";
 export default function App() {
   const [state, setState] = React.useState({
     latitude: 37.78825,
     longitude: -122.4324,
     errorMsg: null, //use to know if we successfully get the user current location
+    destination: "", //the store the input from the textBox
+    predictions: [],
   });
   const mapRef = React.useRef();
 
@@ -41,7 +46,45 @@ export default function App() {
     })();
   }, []);
 
-  const { latitude, longitude } = state;
+  /** This method searches and set the location that the user wants to go to */
+  const onChangeDestination = (destination) => {
+    setState((currState) => ({
+      ...currState,
+      destination,
+    }));
+    delayedQuery(destination);
+  };
+
+  const queryPlace = async (destination) => {
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}
+    &input=${destination}&location=${state.latitude},${state.longitude}&radius=2000`;
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+
+      setState((currState) => ({
+        ...currState,
+        predictions: json.predictions,
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const delayedQuery = React.useCallback(
+    /**
+     * debounce enables us to wait a set period of time before we call the function if the function is not called
+     * with that  set period of time
+     *
+     * Eg we wait for 800ms and if there is not anymore function call since the last call then we run the function
+     * else we wait afresh and wait another 800ms. This helps us not call the place API each time the user is typing
+     *  but instead run the function after 800ms where the user has stop typing which saves us more money :)
+     */
+    _.debounce((q) => queryPlace(q), 800),
+    []
+  );
+
+  const { latitude, longitude, destination, predictions } = state;
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -54,9 +97,22 @@ export default function App() {
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
-      >
-        <Marker coordinate={{ latitude, longitude }} />
-      </MapView>
+        showsUserLocation={true}
+      />
+      <View style={styles.placePickerWrapper}>
+        <TextInput
+          placeholder="Enter destination..."
+          style={styles.destinationInput}
+          value={destination}
+          onChangeText={(destination) => onChangeDestination(destination)}
+        />
+        {predictions.map((prediction) => (
+          <Text style={styles.suggestions} key={prediction.place_id}>
+            {prediction.description}
+          </Text>
+        ))}
+        {/** Dont forget to add a "Powered by Google" logo here. This is a must when using google APIs */}
+      </View>
     </View>
   );
 }
@@ -71,5 +127,27 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  placePickerWrapper: {
+    top: Constants.statusBarHeight,
+    position: "absolute",
+    width: "97%",
+  },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: "white",
+  },
+  suggestions: {
+    backgroundColor: "white",
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5,
   },
 });
