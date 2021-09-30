cfca99a0faceb16546dfac1c9ef4604c03e6b7b7
okpalaChidiebere <okpalacollins4@gmail.com>
2021-09-29 19:27:22 -0600

    Get current user phone Loaction and set Marker on the Map

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [state, setState] = React.useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    errorMsg: null, //use to know if we successfully get the user current location
+  });
+  const mapRef = React.useRef();
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setState((currState) => ({
+          ...currState,
+          errorMsg: "Permission to access location was denied",
+        }));
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+      });
+      setState((currState) => ({
+        ...currState,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      }));
+      mapRef.current.animateToRegion(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        },
+        1000
+      );
+    })();
+  }, []);
+
+  const { latitude, longitude } = state;
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <MapView
+        ref={mapRef}
         style={styles.map}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude,
+          longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
         }}
-      />
+      >
+        <Marker coordinate={{ latitude, longitude }} />
+      </MapView>
     </View>
   );
 }
